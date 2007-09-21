db9fbdef1c8ee19552c29335c85897cd45403bed
James Haley <haleyjd@hotmail.com>
2007-09-21 10:26:07 -0600

    Tweaks to SoM's Cardboard fix and resolution of the other FIXME item.

--- eternity/source/r_bsp.c
+++ eternity/source/r_bsp.c
@@ -945,19 +945,18 @@ static void R_AddLine(seg_t *line)
              (seg.backsec->ceilingpic  == skyflatnum ||
               seg.backsec->ceilingpic  == sky2flatnum));
 
-      if(seg.backsec->ceilingheight <= seg.backsec->floorheight && 
-         ((seg.backsec->ceilingheight != seg.frontsec->ceilingheight && !seg.side->toptexture) ||
-         (seg.backsec->floorheight != seg.frontsec->floorheight && !seg.side->bottomtexture)))
-         ;
-      else
+      if(sky || 
+         !(seg.backsec->ceilingheight <= seg.backsec->floorheight && 
+           ((seg.backsec->ceilingheight != seg.frontsec->ceilingheight && 
+             !seg.side->toptexture) ||
+            (seg.backsec->floorheight != seg.frontsec->floorheight && 
+             !seg.side->bottomtexture))))
       {
-         // FIXME: redundant tests
          seg.clipsolid = 
             (seg.backsec->ceilingheight <= seg.backsec->floorheight || 
              ((seg.frontsec->ceilingheight <= seg.backsec->floorheight || 
-               seg.backsec->ceilingheight <= seg.frontsec->floorheight || 
-               seg.backsec->floorheight >= seg.backsec->ceilingheight) && 
-               !sky));
+               seg.backsec->ceilingheight <= seg.frontsec->floorheight) && 
+              !sky));
       }
 
       if(sky)
