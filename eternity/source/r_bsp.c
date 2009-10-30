92f673d30a8f44dd17175142bb27c35a147df117
James Haley <haleyjd@hotmail.com>
2009-10-30 07:24:13 -0600

    Kate's fix to float->int conversion munge-up in R_AddLine.

--- eternity/source/r_bsp.c
+++ eternity/source/r_bsp.c
@@ -1649,6 +1649,22 @@ static void R_AddLine(seg_t *line)
    seg.x2 = (int)floorx2;
    seg.x2frac = x2;
 
+   // [Kate] Since some really wacky math in part of Visual Studio 2008 can
+   // result in these values being "corrupted" (read: terribly out of range for
+   // an integer), we need to bump them a digit so they're never INT_MIN or
+   // INT_MAX exactly, since R_RenderSegLoop (which is invoked by
+   // R_ClipSolidWallSegment) may accidentally wrap them in it's main loop,
+   // causing it to loop an insane number of times and eventually throw an
+   // Access Violation in R_WindowAdd, which tries to read garbage data. Oops.
+   //
+   // haleyjd 10/30/09: addendum - yes, casting out-of-range floats to ints is 
+   //                   undefined :)
+
+   if(seg.x1 == INT_MIN)
+      seg.x1 = INT_MIN + 1;
+   if(seg.x2 == INT_MAX)
+      seg.x2 = INT_MAX - 1;
+
    if(portalrender.active && portalrender.segClipFunc)
       portalrender.segClipFunc();
    else if(seg.clipsolid)
