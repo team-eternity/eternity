8997ca0202013bef252ccd8b18552f49c6135722
James Haley <haleyjd@hotmail.com>
2010-04-07 20:23:25 -0600

    Define ZONEVERBOSE to re-enable informational but potentially dangerous print-out of block->file and block->line on zone errors.

--- source/z_native.c
+++ source/z_native.c
@@ -70,6 +70,10 @@
 // Uncomment this to log all memory operations to a file
 //#define ZONEFILE
 
+// Uncomment this to enable more verbose - but dangerous - error messages
+// that could cause crashes
+//#define ZONEVERBOSE
+
 //=============================================================================
 //
 // Tunables
@@ -140,7 +144,7 @@ static void Z_IDCheckNB(boolean err, const char *errmsg,
                         const char *file, int line)
 {
    if(err)
-      I_Error("%s\nSource: %s:%d\n",errmsg, file, line);
+      I_Error("%s\nSource: %s:%d\n", errmsg, file, line);
 }
 
 //
@@ -157,7 +161,7 @@ static void Z_IDCheck(boolean err, const char *errmsg,
    {
       I_Error("%s\nSource: %s:%d\nSource of malloc: %s:%d",
               errmsg, file, line,
-#ifdef INSTRUMENTED
+#if defined(ZONEVERBOSE) && defined(INSTRUMENTED)
               block->file, block->line
 #else
               "(not available)", 0
