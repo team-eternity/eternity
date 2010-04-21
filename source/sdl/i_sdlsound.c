8397eb4aea81b3cab03d45b5a0c359cdccb7c9d2
James Haley <haleyjd@hotmail.com>
2010-04-21 01:40:27 -0600

    Experimental implementation of 3-band equalizer with support for bass boost and low pass filter. Currently operates at startup on cached sounds; goal is to move to a runtime postmix effect. Parameters are currently hard-coded for testing purposes.

--- source/sdl/i_sdlsound.c
+++ source/sdl/i_sdlsound.c
@@ -156,6 +156,143 @@ static void stopchan(int handle)
    }
 }
 
+
+typedef struct EQSTATE_s
+{
+  // Filter #1 (Low band)
+
+  double  lf;       // Frequency
+  double  f1p0;     // Poles ...
+  double  f1p1;    
+  double  f1p2;
+  double  f1p3;
+
+  // Filter #2 (High band)
+
+  double  hf;       // Frequency
+  double  f2p0;     // Poles ...
+  double  f2p1;
+  double  f2p2;
+  double  f2p3;
+
+  // Sample history buffer
+
+  double  sdm1;     // Sample data minus 1
+  double  sdm2;     //                   2
+  double  sdm3;     //                   3
+
+  // Gain Controls
+
+  double  lg;       // low  gain
+  double  mg;       // mid  gain
+  double  hg;       // high gain
+  
+} EQSTATE;  
+
+//
+// do_eq
+//
+// haleyjd 04/20/10: 
+//
+static double do_eq(EQSTATE *es, double sample)
+{
+   //static double vsa = (1.0 / 4294967295.0);
+
+   // Locals
+   double  l, m, h;    // Low / Mid / High - Sample Values
+
+   // Filter #1 (lowpass)
+   es->f1p0  += (es->lf * (sample   - es->f1p0)) /*+ vsa*/;
+   es->f1p1  += (es->lf * (es->f1p0 - es->f1p1));
+   es->f1p2  += (es->lf * (es->f1p1 - es->f1p2));
+   es->f1p3  += (es->lf * (es->f1p2 - es->f1p3));
+
+   l          = es->f1p3;
+
+   // Filter #2 (highpass)
+   es->f2p0  += (es->hf * (sample   - es->f2p0)) /*+ vsa*/;
+   es->f2p1  += (es->hf * (es->f2p0 - es->f2p1));
+   es->f2p2  += (es->hf * (es->f2p1 - es->f2p2));
+   es->f2p3  += (es->hf * (es->f2p2 - es->f2p3));
+
+   h          = es->sdm3 - es->f2p3;
+
+   // Calculate midrange (signal - (low + high))
+   //m          = es->sdm3 - (h + l);
+   m          = sample - (h + l);
+
+   // Scale, Combine and store
+   l         *= es->lg;
+   m         *= es->mg;
+   h         *= es->hg;
+
+   // Shuffle history buffer
+   es->sdm3   = es->sdm2;
+   es->sdm2   = es->sdm1;
+   es->sdm1   = sample;                
+
+   // Return result
+   return (l + m + h);
+}
+
+//
+// filter_eq
+//
+// haleyjd 04/20/10: sound equalization filter
+//
+static void filter_eq(sfxinfo_t *sfx)
+{
+   unsigned int i;
+   byte *data;
+
+   double lowfreq = 880.0, highfreq = 5000.0; // TODO: dynamic?
+   EQSTATE es;
+
+   // Clear state
+   memset(&es, 0, sizeof(EQSTATE));
+
+   // Set Low/Mid/High gains to unity -- TODO: dynamic
+   es.lg = 1.2;
+   es.mg = 1.0;
+   es.hg = 0.8;
+
+   // Calculate filter cutoff frequencies
+   es.lf = 2 * sin(3.14159265 * (lowfreq  / (double)snd_samplerate));
+   es.hf = 2 * sin(3.14159265 * (highfreq / (double)snd_samplerate));
+
+   data = (byte *)(sfx->data);
+
+   for(i = 0; i < sfx->alen; ++i)
+   {
+      double sample = (double)((int)data[i] - 128) / 136.0;
+
+      do_eq(&es, sample);
+
+      sample = sample * 128.0 + 128.0;
+
+      if(sample > 255.0)
+         sample = 255.0;
+      else if(sample < 0.0)
+         sample = 0.0;
+
+      data[i] = (byte)(sample);
+   }
+
+   /*
+   for(i = 0; i < sfx->alen; ++i)
+   {
+      double sample = do_eq(&es, data[i]);
+
+      if(sample > 255.0)
+         sample = 255.0;
+      else if(sample < 0.0)
+         sample = 0.0;
+
+      data[i] = (byte)sample;
+   }
+   */
+}
+
 #define SOUNDHDRSIZE 8
 
 //
@@ -269,6 +406,8 @@ static boolean addsfx(sfxinfo_t *sfx, int channel, int loop, unsigned int id)
          // sound is already at target samplerate, copy data
          memcpy(sfx->data, data + SOUNDHDRSIZE, samplelen);
       }
+
+      filter_eq(sfx);
 
       // haleyjd 06/03/06: don't need original lump data any more
       Z_ChangeTag(data, PU_CACHE);
