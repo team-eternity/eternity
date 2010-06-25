51be18f9697a34387c897bb22f1ac2d915c8e36a
James Haley <haleyjd@hotmail.com>
2010-06-24 22:43:13 -0600

    Fix the rest of the hundred or so C++ mode selects that the global replace operation ignored for whatever reason.

--- source/m_fcvt.h
+++ source/m_fcvt.h
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*- 
+// Emacs style mode select   -*- C -*- 
 //-----------------------------------------------------------------------------
 //
 // Copyright (C) 2002 DJ Delorie, see COPYING.DJ for details 
--- source/sdl/i_pcsound.c
+++ source/sdl/i_pcsound.c
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*-
+// Emacs style mode select   -*- C -*-
 //-----------------------------------------------------------------------------
 //
 // Copyright(C) 2008 James Haley, Stephen McGranahan, Simon Howard, et al.
