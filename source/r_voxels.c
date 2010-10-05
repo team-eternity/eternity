f23cd90609b75db95ca5c000293c3532e98f2a17
James Haley <haleyjd@hotmail.com>
2010-10-05 11:58:52 -0600

    Loading function for .vox format voxel models.

--- source/r_voxels.c
+++ source/r_voxels.c
@@ -23,7 +23,73 @@
 //
 //-----------------------------------------------------------------------------
 
+#include "z_zone.h"
+#include "doomtype.h"
+#include "m_swap.h"
+#include "w_wad.h"
+
 #include "r_voxels.h"
+
+//
+// R_LoadVoxelResource
+//
+// Loads a .vox-format voxel model into an rvoxelmodel_t structure.
+//
+rvoxelmodel_t *R_LoadVoxelResource(int lumpnum)
+{
+   rvoxelmodel_t *model = NULL;
+   byte *buffer = NULL, *rover = NULL;
+   int lumplen  = W_LumpLength(lumpnum);
+   int xsize, ysize, zsize, voxsize;
+   int i;
+
+   // minimum size test
+   if(lumplen < 12)
+      return NULL;
+
+   // cache the lump
+   rover = buffer = W_CacheLumpNum(lumpnum, PU_STATIC);
+
+   // get sizes
+   xsize = SwapLong(*(int32_t *)rover);
+   rover += 4;
+   ysize = SwapLong(*(int32_t *)rover);
+   rover += 4;
+   zsize = SwapLong(*(int32_t *)rover);
+   rover += 4;
+
+   voxsize = xsize*ysize*zsize;
+
+   // true size test
+   if(lumplen < 12 + voxsize + 768)
+   {
+      Z_ChangeTag(buffer, PU_CACHE);
+      return NULL;
+   }
+
+   // create the model and its voxel buffer
+   model         = Z_Calloc(1,       sizeof(rvoxelmodel_t), PU_RENDERER, NULL);
+   model->voxels = Z_Calloc(voxsize, sizeof(byte),          PU_RENDERER, NULL);
+
+   // get voxel data
+   memcpy(model->voxels, rover, voxsize);
+   rover += voxsize;
+
+   // get original palette data
+   memcpy(model->palette, rover, 768);
+
+   // transform palette data into 0-255 range
+   // TODO: verify color component order
+   for(i = 0; i < 768; i++)
+      model->palette[i] <<= 2;
+
+   // TODO: run V_FindBestColor to create translated palette?
+
+   // done with lump
+   Z_ChangeTag(buffer, PU_CACHE);
+
+   return model;
+}
 
 // EOF
 
