a89b4e69eeed98cac061c78c34a73f3f51ed6782
Charles Gunyon <ladna@intldoomleague.net>
2011-08-15 21:02:48 +0000

    Increase the maximum latency tolerated to an insane amount, should prevent players on spiky connections from disconnecting.

--- source/cs_main.h
+++ source/cs_main.h
@@ -64,7 +64,7 @@
 //      exceeds half of this it will be disconnected, even if none of the other
 //      connected peers have latencies <= (MAX_LATENCY / 2).  This is in
 //      seconds.
-#define MAX_LATENCY 2
+#define MAX_LATENCY 10
 
 // [CG] The default port the server listens on if none is given.
 #define DEFAULT_PORT 10666
