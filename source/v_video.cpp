0bf2ad77fa2bf3f8b35e9b86b8fb58a48b5463a0
James Haley <haleyjd@hotmail.com>
2012-05-07 18:48:18 -0600

    Insert ZDoom BSD-alike license into files where it applies to some or all of the code within.

--- source/v_video.cpp
+++ source/v_video.cpp
@@ -806,6 +806,35 @@ byte V_FindBestColor(const byte *palette, int r, int g, int b)
 // additive translucency. Note this code is included in Odamex and
 // so it can be considered GPL as used here, rather than BSD. But,
 // I don't care either way. It is effectively dual-licensed I suppose.
+// So, you can use it under this license if you wish:
+//
+// Copyright 1998-2012 Randy Heit  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 3. The name of the author may not be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 bool flexTranInit = false;
 unsigned int  Col2RGB8[65][256];
@@ -899,9 +928,6 @@ void V_CacheBlock(int x, int y, int width, int height, byte *src,
       dest += SCREENWIDTH;
    }
 }
-
-
-
 
 //----------------------------------------------------------------------------
 //
