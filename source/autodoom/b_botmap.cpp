ed844ca111e01e3c36d10c5c06036f164c127b30
Ioan Chera <ioan_chera@yahoo.com>
2014-09-09 20:14:06 +0300

    Barrels are targetted again

--- source/autodoom/b_botmap.cpp
+++ source/autodoom/b_botmap.cpp
@@ -506,7 +506,7 @@ void BotMap::getAllLivingMonsters()
     for (th = thinkercap.next; th != &thinkercap; th = th->next)
     {
         mo = thinker_cast<const Mobj*>(th);
-        if (mo && !mo->player && mo->flags & MF_SHOOTABLE && !(mo->flags & MF_NOBLOCKMAP) && mo->health > 0 && B_IsMobjHostile(*mo))
+        if (mo && !mo->player && mo->flags & MF_SHOOTABLE && !(mo->flags & MF_NOBLOCKMAP) && mo->health > 0)
         {
             livingMonsters.add(mo);
         }
