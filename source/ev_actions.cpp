e847d02ce3c486c5dd8f202a00b5da69d478c60a
Ioan Chera <ioan_chera@yahoo.com>
2016-07-27 09:23:27 +0300

    Use instance->args, not instance->line->args, in ChangeSkill

--- source/ev_actions.cpp
+++ source/ev_actions.cpp
@@ -3876,7 +3876,7 @@ DEFINE_ACTION(EV_ActionParamElevatorCurrent)
 DEFINE_ACTION(EV_ActionChangeSkill)
 {
    // Sanity check the argument, skills 0 through 4 are valid
-   if(instance->line->args[0] < sk_baby || instance->line->args[0] > sk_nightmare)
+   if(instance->args[0] < sk_baby || instance->args[0] > sk_nightmare)
       return 0;
 
    gameskill = (skill_t)instance->line->args[0];
