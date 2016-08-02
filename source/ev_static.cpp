e0a2915cd0b6863a2d1ac621faa218b44f202fb8
Ioan Chera <ioan_chera@yahoo.com>
2016-08-03 01:28:26 +0300

    Added Transfer_FloorLight Hexen/UDMF binding for the Boom special.

--- source/ev_static.cpp
+++ source/ev_static.cpp
@@ -152,6 +152,7 @@ static ev_static_t HexenStaticBindings[] =
    STATICSPEC(103, EV_STATIC_SCROLL_DOWN_PARAM)
    STATICSPEC(121, EV_STATIC_LINE_SET_IDENTIFICATION)
    STATICSPEC(181, EV_STATIC_SLOPE_PARAM)
+   STATICSPEC(210, EV_STATIC_LIGHT_TRANSFER_FLOOR)
 };
 
 // PSX Static Init Bindings
@@ -570,6 +571,7 @@ int EV_SpecialForStaticInitName(const char *name)
       { EV_STATIC_PORTAL_SECTOR_PARAM,     "Sector_SetPortal"       },
       { EV_STATIC_PORTAL_HORIZON_LINE,     "Line_Horizon"           },
       { EV_STATIC_LINE_SET_IDENTIFICATION, "Line_SetIdentification" },
+      { EV_STATIC_LIGHT_TRANSFER_FLOOR,    "Transfer_FloorLight"    },
    };
 
    // There aren't enough of these to warrant a hash table. Yet.
