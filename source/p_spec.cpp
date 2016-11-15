52af87c37b90d49ae6272e7c290d32e2524d580f
Ioan Chera <ioan.chera@gmail.com>
2016-11-15 21:07:58 +0200

    Deleted Sector_SetAnchoredPortal
    Same reason as before: Portal_Define

--- source/p_spec.cpp
+++ source/p_spec.cpp
@@ -1350,11 +1350,6 @@ void P_SpawnSpecials(UDMFSetupSettings &setupSettings)
             lines[i].args[2] << FRACBITS);
          break;
 
-      case EV_STATIC_PORTAL_SECTOR_PARAM_ANCHORED:
-         R_SpawnAnchoredSectorPortal(lines[i], lines[i].args[0], lines[i].args[1],
-            lines[i].args[2], lines[i].args[3], lines[i].args[4] << FRACBITS);
-         break;
-
       case EV_STATIC_PORTAL_SECTOR_PARAM_LINKED:
          R_SpawnLinkedSectorPortal(lines[i], lines[i].args[0], lines[i].args[1],
             lines[i].args[2], lines[i].args[3], lines[i].args[4] << FRACBITS);
