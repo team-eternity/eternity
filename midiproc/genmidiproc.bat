62091c6176ace807348f1f3d0f27ddf8a566ee1f
Max Waine <bobishmax@hotmail.com>
2016-12-21 15:46:52 +0000

    Fixed and improved the midiproc gen script
    Now lets you choose from the 4 build environments available.
    win64 and ia64 are the same for the MIDL compiler.

--- midiproc/genmidiproc.bat
+++ midiproc/genmidiproc.bat
@@ -1,19 +1,26 @@
 @ECHO OFF
 ECHO THIS MUST BE RUN FROM THE DEVELOPER COMMAND PROMPT FOR VS2015
-ECHO 1. 32-bit
-ECHO 2. 64-bit
+ECHO Choose your environment:
+ECHO 1. win32
+ECHO 2. ia64/win64
+ECHO 3. amd64
 SET SELECTION=
 SET /P SELECTION=Enter selection: %=%
-IF %SELECTION%=="1"  GOTO win32
-IF %SELECTION%=="2"  GOTO win64
+IF %SELECTION%==1  GOTO win32
+IF %SELECTION%==2  GOTO ia64
+IF %SELECTION%==3  GOTO amd64
 
 :win32
-ECHO Building Win32
+ECHO Building win32
 midl -Oicf -W1 -Zp8 /env win32 /protocol dce /app_config midiproc.idl
 EXIT /B
-:win64
-ECHO Building Win64
-midl -Oicf -W1 -Zp8 /env win64 /protocol dce /app_config midiproc.idl
+:ia64
+ECHO Building ia64/win64
+midl -Oicf -W1 -Zp8 /env ia64 /protocol dce /app_config midiproc.idl
+EXIT /B
+:amd64
+ECHO Building amd64
+midl -Oicf -W1 -Zp8 /env amd64 /protocol dce /app_config midiproc.idl
 EXIT /B
 
 REM This command should be similar to what was initially used to generate the old midiproc
