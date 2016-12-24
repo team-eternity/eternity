509c6f59db60e91c2bb76aea57cc302844b6ad3e
Max Waine <bobishmax@hotmail.com>
2016-12-24 03:36:33 +0000

    Update vc2015 files; fix compilation on msvc2015

--- acsvm/ACSVM/Environment.cpp
+++ acsvm/ACSVM/Environment.cpp
@@ -406,7 +406,8 @@ namespace ACSVM
                throw std::bad_alloc();
             #endif
 
-            idx = new PrivData::FuncElem{std::move(namePair), pd->functionByIdx.size()};
+            idx = new PrivData::FuncElem{ std::move(namePair),
+                                          static_cast<Word>(pd->functionByIdx.size()) };
             pd->functionByName.insert(idx);
 
             pd->functionByIdx.emplace_back();
