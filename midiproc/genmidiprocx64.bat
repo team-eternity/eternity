fac9a15c0a73e771e77eebc37e4bc60cf7d72163
Max Waine <bobishmax@hotmail.com>
2017-03-03 12:07:39 +0000

    Fix midl compiling for users with spaces in names
    And also in general fixes if the eternity directory has a parent dir with
    spaces. Fixes #149

--- midiproc/genmidiprocx64.bat
+++ midiproc/genmidiprocx64.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 CALL "%VS140COMNTOOLS%\VsDevCmd.bat"
 ECHO Building amd64
-midl -Oicf -W1 -Zp8 /env amd64 /protocol dce /app_config /out %~dp0 %~dp0midiproc.idl
+midl -Oicf -W1 -Zp8 /env amd64 /protocol dce /app_config /out ""%~dp0\"" ""%~dp0midiproc.idl""
 EXIT /B
 
 REM This command should be similar to what was initially used to generate the old midiproc
