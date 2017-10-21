84e138cb2485d94b0ca27ff433883b5cbf35708d
Max Waine <bobishmax@hotmail.com>
2017-10-21 21:16:43 +0100

    Update version name

--- source/version.cpp
+++ source/version.cpp
@@ -36,11 +36,11 @@ const char version_date[] = __DATE__;
 const char version_time[] = __TIME__; // haleyjd
 
 // sf: version name -- at the suggestion of mystican
-const char version_name[] = "Heimdal";
+const char version_name[] = u8"Völuspá";
 
 // haleyjd: caption for SDL window
 #ifdef _SDL_VER
-const char ee_wmCaption[] = "Eternity Engine v3.43.00 \"Heimdal\"";
+const char ee_wmCaption[] = u8"Eternity Engine v3.43.00 \"Völuspá\"";
 #endif
             
 // haleyjd: Eternity release history
