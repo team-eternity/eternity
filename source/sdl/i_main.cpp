7d0c86ac8aecedef4988437be612377a57372cb2
Ioan Chera <ioan.chera@gmail.com>
2017-12-22 21:48:36 +0200

    macos: added demolog cpp file and prevent SDL_VIDEO in case of demo testing.

--- source/sdl/i_main.cpp
+++ source/sdl/i_main.cpp
@@ -70,7 +70,10 @@ int main(int argc, char **argv)
 
    // MaxW: 2017/09/16: Now prints the error on failure
    // haleyjd 04/15/02: added check for failure
-   if(SDL_Init(INIT_FLAGS) == -1)
+   // ioanch: avoid loading SDL_VIDEO if -nodraw and -demolog are combined.
+   Uint32 initflags = (M_CheckParm("-nodraw") && M_CheckParm("-demolog")) ?
+                      SDL_INIT_JOYSTICK : SDL_INIT_VIDEO | SDL_INIT_JOYSTICK;
+   if(SDL_Init(initflags) == -1)
    {
       printf("Failed to initialize SDL library: %s\n", SDL_GetError());
       return -1;
