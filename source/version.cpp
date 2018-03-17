0209d7d8ac1673e324b99a28ba2c73b1d3b5994e
Max Waine <bobishmax@hotmail.com>
2018-03-17 20:18:58 +0000

    Update version release history

--- source/version.cpp
+++ source/version.cpp
@@ -80,6 +80,7 @@ const char ee_wmCaption[] = u8"Eternity Engine v4.00.00 \"Völuspá\"";
 // 3.40.46 'Bifrost'   -- 01/19/14
 // 3.42.02 'Heimdal'   -- 05/07/17
 // 3.42.03 'Heimdal 2' -- 08/02/17
+// 4.00.00 'Völuspá'   -- 03/17/18
 
 // auxilliary releases
 // Caverns of Darkness -- 3.29 dev beta 5 joel-2 04/24/02
