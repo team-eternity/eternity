0aa346c90495360b6c81450b56cab3aaf38d73cd
Ioan Chera <ioan.chera@gmail.com>
2018-09-02 18:17:29 +0300

    Removed ... from Saving...

--- source/autodoom/b_botmap.cpp
+++ source/autodoom/b_botmap.cpp
@@ -655,7 +655,7 @@ void BotMap::cacheToFile(const char* path) const
                                 segBlocks.getLength() + lineBlocks.getLength());
    int progress = 0;
 
-   V_SetLoading(total, "Saving...");
+   V_SetLoading(total, "Saving");
     
     try
     {
