91ba1480e861532b2cc7dfe2acfbe3c37fe0dbea
Ioan Chera <ioan.chera@gmail.com>
2019-02-13 22:43:29 +0200

    Fixed texture translation not being taken into account when deciding to shortcircuit blendflags to 0

--- source/r_plane.cpp
+++ source/r_plane.cpp
@@ -654,8 +654,8 @@ visplane_t *R_FindPlane(fixed_t height, int picnum, int lightlevel,
    blendflags &= PS_OBLENDFLAGS;
 
    // haleyjd: tweak opacity/blendflags when 100% opaque is specified
-   if(!(blendflags & PS_ADDITIVE) && opacity == 255 && (picnum & PL_SKYFLAT ||
-                                                        !(textures[picnum]->flags & TF_MASKED)))
+   if(!(blendflags & PS_ADDITIVE) && opacity == 255 &&
+      (picnum & PL_SKYFLAT || !(textures[texturetranslation[picnum]]->flags & TF_MASKED)))
    {
       blendflags = 0;
    }
