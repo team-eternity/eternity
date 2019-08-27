3d8c98b96ed2e5ad9f6ba837348e4894640ea281
Ioan Chera <ioan.chera@gmail.com>
2019-08-27 08:52:26 +0300

    Added timer
    Also put show-endoom into GUI's resources

--- macosx/show-endoom/main.cpp
+++ macosx/show-endoom/main.cpp
@@ -137,7 +137,7 @@ int main(int argc, const char * argv[])
       TXT_UpdateScreen();
       if(TXT_GetChar() > 0)
          break;
-      TXT_Sleep(0);
+      TXT_Sleep(1);
    }
 
    TXT_Shutdown();
