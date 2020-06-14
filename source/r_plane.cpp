2e036093ecde1290cd23ada7d38f97ecd1f3e9a5
Ioan Chera <ioan.chera@gmail.com>
2020-06-14 11:43:48 +0300

    Fixed crash caused by bad assumption which in turn was caused by badly written MBF code
    The problem being that visplane picnum was overloaded to also mean PL_SKYFLAT + index, which is crash-prone if you don't know this in advance.

--- source/r_plane.cpp
+++ source/r_plane.cpp
@@ -663,12 +663,9 @@ visplane_t *R_FindPlane(fixed_t height, int picnum, int lightlevel,
       
    blendflags &= PS_OBLENDFLAGS;
 
-   // NOTE: this works because we still are limited to PO2 flats.
-   const texture_t *texture = textures[texturetranslation[picnum]];
-
    // haleyjd: tweak opacity/blendflags when 100% opaque is specified
    if(!(blendflags & PS_ADDITIVE) && opacity == 255 &&
-      (picnum & PL_SKYFLAT || !(texture->flags & TF_MASKED)))
+      (picnum & PL_SKYFLAT || !(textures[texturetranslation[picnum]]->flags & TF_MASKED)))
    {
       blendflags = 0;
    }
