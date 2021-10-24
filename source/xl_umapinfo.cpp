dea5be84b9ecd3f14483b9c0130cb55735b6d1a0
Ioan Chera <ioan.chera@gmail.com>
2021-10-24 18:59:06 +0300

    Added an UMAPINFO grammar validator
    It will I_Error if it's violated. It's needed to make it easier to manage the more complex parameters.

--- source/xl_umapinfo.cpp
+++ source/xl_umapinfo.cpp
@@ -22,13 +22,44 @@
 // Authors: Ioan Chera
 //
 
+#include <assert.h>
 #include "z_zone.h"
 
+#include "e_hash.h"
 #include "in_lude.h"
 #include "metaqstring.h"
 #include "w_wad.h"
 #include "xl_scripts.h"
 #include "xl_umapinfo.h"
+
+//
+// UMAPINFO property data type
+//
+enum class PropertyType
+{
+   setString,  // assign a string (replacing anything before it)
+   setStringOrClear, // assign a string or accept 'clear'
+   setMultiStringOrClear,  // assign one or more strings, or 'clear'
+   setInteger, // assign an integer number (replacing anything before it)
+   setBoolean, // true or false
+   setTrue, // only accept 'true'
+   addEpisodeOrClear, // accept three strings, or a single 'clear'
+   addBossActionOrClear,   // accept <identifier>, <int>, <int> or 'clear'
+};
+
+//
+// UMAPINFO property rule
+//
+struct propertyrule_t
+{
+   // hash table items
+   const char *name;
+   DLListItem<propertyrule_t> link;
+
+   PropertyType type;
+};
+
+static EHashTable<propertyrule_t, ENCStringHashKey, &propertyrule_t::name, &propertyrule_t::link> rules;
 
 // UMAPINFO entry maintenance
 static MetaTable umapInfoTable;
@@ -83,6 +114,8 @@ class XLUMapInfoParser final : public XLParser
    MetaTable *curInfo;  // level info
    qstring key;         // key string
    qstring value;       // value string
+   int valueIndex;      // index of value in a multi-parameter option
+   bool gotClear;       // whether the value got 'clear'
 
    bool doToken(XLTokenizer &token) override;
    void startLump() override;
@@ -165,7 +198,17 @@ bool XLUMapInfoParser::doStateExpectKey(XLTokenizer &tokenizer)
       state = STATE_EXPECTMAP;
       return true;
    }
-   key = tokenizer.getToken();
+
+   // Check the key
+   const qstring &newkey = tokenizer.getToken();
+   if(!rules.objectForKey(newkey.constPtr()))
+   {
+      // Don't allow unrecognized properties
+      I_Error("UMAPINFO: unexpected property '%s' in map '%s'\n", newkey.constPtr(),
+              curInfo->getKey());
+   }
+
+   key = newkey;
    value = "";
    state = STATE_EXPECTEQUAL;
    return true;
@@ -178,6 +221,8 @@ bool XLUMapInfoParser::doStateExpectEqual(XLTokenizer &tokenizer)
 {
    if(tokenizer.getToken() == "=")
    {
+      valueIndex = 0;
+      gotClear = false;
       state = STATE_EXPECTVALUE;
       return true;
    }
@@ -192,27 +237,153 @@ bool XLUMapInfoParser::doStateExpectEqual(XLTokenizer &tokenizer)
 bool XLUMapInfoParser::doStateExpectValue(XLTokenizer &tokenizer)
 {
    value = tokenizer.getToken();
+   int type = tokenizer.getTokenType();
 
-   if(tokenizer.getTokenType() == XLTokenizer::TOKEN_STRING)
-      curInfo->addString(key.constPtr(), value.constPtr());
-   else if(tokenizer.getTokenType() == XLTokenizer::TOKEN_KEYWORD)
+   const propertyrule_t *rule = rules.objectForKey(key.constPtr());
+   I_Assert(rule, "Key '%s' unexpectedly passed\n", key.constPtr());
+
+   //
+   // Helper to check if it's a number
+   //
+   auto isNumber = [this, type]()
    {
-      char *endptr = nullptr;
-      long val = value.toLong(&endptr, 10);
-      if(!*endptr)
-         curInfo->addInt(key.constPtr(), (int)val);
-      else if(!value.strCaseCmp("clear"))
-         curInfo->addInt(key.constPtr(), XL_UMAPINFO_SPECVAL_CLEAR);
-      else if(!value.strCaseCmp("false"))
-         curInfo->addInt(key.constPtr(), XL_UMAPINFO_SPECVAL_FALSE);
-      else if(!value.strCaseCmp("true"))
-         curInfo->addInt(key.constPtr(), XL_UMAPINFO_SPECVAL_TRUE);
-      else
-      {
-         I_Error("UMAPINFO: unexpected token '%s' for key '%s' in map '%s'\n",
-                 value.constPtr(), key.constPtr(), curInfo->getKey());
-         return false;  // FAILURE
-      }
+      if(type != XLTokenizer::TOKEN_KEYWORD)
+         return false;
+      char *endptr;
+      strtol(value.constPtr(), &endptr, 10);
+      return !*endptr;
+   };
+
+   // Check the data type
+   bool error = false;
+   switch(rule->type)
+   {
+      case PropertyType::setString:
+         error = valueIndex >= 1 || type != XLTokenizer::TOKEN_STRING;
+         break;
+      case PropertyType::setStringOrClear:
+         error = valueIndex >= 1 || (type != XLTokenizer::TOKEN_STRING &&
+                                     value.strCaseCmp("clear"));
+         break;
+      case PropertyType::setMultiStringOrClear:
+         error = (gotClear && valueIndex >= 1) || (valueIndex >= 1 && type !=
+                                                   XLTokenizer::TOKEN_STRING) ||
+         (valueIndex == 0 && type != XLTokenizer::TOKEN_STRING && value.strCaseCmp("clear"));
+         break;
+      case PropertyType::setInteger:
+         error = valueIndex >= 1 || !isNumber();
+         break;
+      case PropertyType::setBoolean:
+         error = valueIndex >= 1 || type != XLTokenizer::TOKEN_KEYWORD ||
+         (value.strCaseCmp("true") && value.strCaseCmp("false"));
+         break;
+      case PropertyType::setTrue:
+         error = valueIndex >= 1 || type != XLTokenizer::TOKEN_KEYWORD || value.strCaseCmp("true");
+         break;
+      case PropertyType::addEpisodeOrClear:
+         error = (gotClear && valueIndex >= 1) ||
+         (valueIndex >= 1 && type != XLTokenizer::TOKEN_STRING) || valueIndex >= 3 ||
+         (valueIndex == 0 && type != XLTokenizer::TOKEN_STRING && value.strCaseCmp("clear"));
+         break;
+      case PropertyType::addBossActionOrClear:
+         error = (gotClear && valueIndex >= 1) || type != XLTokenizer::TOKEN_KEYWORD ||
+         valueIndex >= 3 || (valueIndex >= 1 && !isNumber());
+         break;
+   }
+   if(error)
+   {
+      I_Error("UMAPINFO: invalid value for property '%s' for map '%s'\n", key.constPtr(),
+              curInfo->getKey());
+   }
+
+   // Now that validation is out of our way, do the assignment
+   MetaTable *subtable;
+   switch(rule->type)
+   {
+      case PropertyType::setString:
+         curInfo->setString(key.constPtr(), value.constPtr());
+         break;
+      case PropertyType::setStringOrClear:
+         if(type == XLTokenizer::TOKEN_KEYWORD)
+         {
+            curInfo->removeString(key.constPtr());
+            curInfo->setInt(key.constPtr(), XL_UMAPINFO_SPECVAL_CLEAR);
+            gotClear = true;
+         }
+         else
+         {
+            curInfo->removeInt(key.constPtr());
+            curInfo->setString(key.constPtr(), value.constPtr());
+         }
+         break;
+      case PropertyType::setMultiStringOrClear:
+         if(type == XLTokenizer::TOKEN_KEYWORD)
+         {
+            curInfo->removeAndDeleteAllObjects(key.constPtr());
+            curInfo->setInt(key.constPtr(), XL_UMAPINFO_SPECVAL_CLEAR);
+            gotClear = true;
+         }
+         else
+         {
+            if(!valueIndex)
+               curInfo->removeAndDeleteAllObjects(key.constPtr());
+            curInfo->addString(key.constPtr(), value.constPtr());
+         }
+         break;
+      case PropertyType::setInteger:
+         curInfo->setInt(key.constPtr(), value.toInt());
+         break;
+      case PropertyType::setBoolean:
+         curInfo->setInt(key.constPtr(), !value.strCaseCmp("false") ? XL_UMAPINFO_SPECVAL_FALSE :
+                         XL_UMAPINFO_SPECVAL_TRUE);
+         break;
+      case PropertyType::setTrue:
+         curInfo->setInt(key.constPtr(), XL_UMAPINFO_SPECVAL_TRUE);
+         break;
+      case PropertyType::addEpisodeOrClear:
+         if(!valueIndex)
+         {
+            if(!value.strCaseCmp("clear"))
+               curInfo->addInt(key.constPtr(), XL_UMAPINFO_SPECVAL_CLEAR);
+            else
+            {
+               subtable = new MetaTable;
+               subtable->setString("patch", value.constPtr());
+               curInfo->addMetaTable(key.constPtr(), subtable);
+            }
+         }
+         else
+         {
+            subtable = curInfo->getMetaTable(key.constPtr(), nullptr);
+            I_Assert(subtable, "Expected to have a table here");
+            if(valueIndex == 1)
+               subtable->setString("name", value.constPtr());
+            else if(valueIndex == 2)
+               subtable->setString("key", value.constPtr());
+         }
+         break;
+      case PropertyType::addBossActionOrClear:
+         if(!valueIndex)
+         {
+            if(!value.strCaseCmp("clear"))
+               curInfo->addInt(key.constPtr(), XL_UMAPINFO_SPECVAL_CLEAR);
+            else
+            {
+               subtable = new MetaTable;
+               subtable->setString("thingtype", value.constPtr());
+               curInfo->addMetaTable(key.constPtr(), subtable);
+            }
+         }
+         else
+         {
+            subtable = curInfo->getMetaTable(key.constPtr(), nullptr);
+            I_Assert(subtable, "Expected to have a table here");
+            if(valueIndex == 1)
+               subtable->setInt("linespecial", value.toInt());
+            else if(valueIndex == 2)
+               subtable->setInt("tag", value.toInt());
+         }
+         break;
    }
 
    state = STATE_POSTVALUE;
@@ -224,8 +395,10 @@ bool XLUMapInfoParser::doStateExpectValue(XLTokenizer &tokenizer)
 //
 bool XLUMapInfoParser::doStatePostValue(XLTokenizer &tokenizer)
 {
+   // NOTE: the error checking will happen in the next value, if a comma is added
    if(tokenizer.getToken() == ",")
    {
+      ++valueIndex;
       state = STATE_EXPECTVALUE;
       return true;
    }
@@ -288,10 +461,52 @@ MetaTable *XL_UMapInfoForMapName(const char *mapname)
 }
 
 //
+// Initialize the rules
+//
+static void XL_initRules()
+{
+   if(rules.getNumItems())
+      return;
+
+   auto addRule = [](const char *name, PropertyType type)
+   {
+      propertyrule_t *rule;
+      rule = estructalloc(propertyrule_t, 1);
+      rule->name = name;
+      rule->type = type;
+      rules.addObject(rule);
+   };
+
+   addRule("levelname", PropertyType::setString);
+   addRule("label", PropertyType::setStringOrClear);
+   addRule("levelpic", PropertyType::setString);
+   addRule("next", PropertyType::setString);
+   addRule("nextsecret", PropertyType::setString);
+   addRule("skytexture", PropertyType::setString);
+   addRule("music", PropertyType::setString);
+   addRule("exitpic", PropertyType::setString);
+   addRule("enterpic", PropertyType::setString);
+   addRule("partime", PropertyType::setInteger);
+   addRule("endgame", PropertyType::setBoolean);
+   addRule("endpic", PropertyType::setString);
+   addRule("endbunny", PropertyType::setTrue);
+   addRule("endcast", PropertyType::setTrue);
+   addRule("nointermission", PropertyType::setBoolean);
+   addRule("intertext", PropertyType::setStringOrClear);
+   addRule("intertextsecret", PropertyType::setStringOrClear);
+   addRule("interbackdrop", PropertyType::setString);
+   addRule("intermusic", PropertyType::setString);
+   addRule("episode", PropertyType::addEpisodeOrClear);
+   addRule("bossaction", PropertyType::addBossActionOrClear);
+}
+
+//
 // Parses the UMAPINFO lump
 //
 void XL_ParseUMapInfo()
 {
+   XL_initRules();
+
    XLUMapInfoParser parser;
    parser.parseAll(wGlobalDir);
 }
