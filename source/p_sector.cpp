a302c9f6be531490381b5d933e01d0a1d605ebc5
Max Waine <bobishmax@hotmail.com>
2022-07-22 01:05:53 +0100

    Fix single-surface P_SaveSectorPosition

--- source/p_sector.cpp
+++ source/p_sector.cpp
@@ -300,10 +300,14 @@ void P_SaveSectorPosition(const sector_t &sec, ssurftype_e surf)
       case ssurf_floor:
          si.prevfloorheight = sec.srf.floor.height;
          si.prevfloorheightf = sec.srf.floor.heightf;
+         if(sec.srf.floor.slope)
+            si.prevfloorslopezf = sec.srf.floor.slope->of.z;
          break;
       case ssurf_ceiling:
          si.prevceilingheight = sec.srf.ceiling.height;
          si.prevceilingheightf = sec.srf.ceiling.heightf;
+         if(sec.srf.ceiling.slope)
+            si.prevceilingslopezf = sec.srf.ceiling.slope->of.z;
          break;
    }
 }
