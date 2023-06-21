3b91220da20062dddfd1ee186f90e4ca45a8299c
Max Waine <bobishmax@hotmail.com>
2023-06-21 22:21:27 +1200

    Fix skew z offset calculation

--- source/r_bsp.cpp
+++ source/r_bsp.cpp
@@ -29,6 +29,7 @@
 #include "doomstat.h"
 #include "e_exdata.h"
 #include "m_bbox.h"
+#include "m_compare.h"
 #include "p_chase.h"
 #include "p_maputl.h"   // ioanch 20160125
 #include "p_portal.h"
@@ -1343,14 +1344,20 @@ static void R_2S_Sloped(cmapcontext_t &cmapcontext, planecontext_t &planecontext
          (!seg.line->frontside && seg.side->topSkewType() == SKEW_SOLID_FRONT))
       {
          seg.topzstep = zstep;
-         seg.topz = realz1 > realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.topz = emin(realz1, realz2);
+         else
+            seg.topz = emax(realz1, realz2);
       }
 
       if((seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_BACK_CEILING) ||
          (!seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_FRONT_CEILING))
       {
          seg.middlezstep = zstep;
-         seg.middlez = realz1 > realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.middlez = emin(realz1, realz2);
+         else
+            seg.middlez = emax(realz1, realz2);
       }
 
 
@@ -1386,14 +1393,20 @@ static void R_2S_Sloped(cmapcontext_t &cmapcontext, planecontext_t &planecontext
          (!seg.line->frontside && seg.side->bottomSkewType() == SKEW_SOLID_FRONT))
       {
          seg.bottomzstep = zstep;
-         seg.bottomz = realz1 < realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.bottomz = emin(realz1, realz2);
+         else
+            seg.bottomz = emax(realz1, realz2);
       }
 
       if((seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_BACK_FLOOR) ||
          (!seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_FRONT_FLOOR))
       {
          seg.middlezstep = zstep;
-         seg.middlez = realz1 < realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.middlez = emin(realz1, realz2);
+         else
+            seg.middlez = emax(realz1, realz2);
       }
    }
    else
@@ -2604,14 +2617,20 @@ static void R_addLine(bspcontext_t &bspcontext, cmapcontext_t &cmapcontext, plan
          (!seg.line->frontside && seg.side->topSkewType() == SKEW_SOLID_BACK))
       {
          seg.topzstep = zstep;
-         seg.topz = realz1 > realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.topz = emin(realz1, realz2);
+         else
+            seg.topz = emax(realz1, realz2);
       }
 
       if((seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_FRONT_CEILING) ||
          (!seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_BACK_CEILING))
       {
          seg.middlezstep = zstep;
-         seg.middlez = realz1 > realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.middlez = emin(realz1, realz2);
+         else
+            seg.middlez = emax(realz1, realz2);
       }
    }
    else
@@ -2642,14 +2661,20 @@ static void R_addLine(bspcontext_t &bspcontext, cmapcontext_t &cmapcontext, plan
          (!seg.line->frontside && seg.side->bottomSkewType() == SKEW_SOLID_BACK))
       {
          seg.bottomzstep = zstep;
-         seg.bottomz = realz1 < realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.bottomz = emin(realz1, realz2);
+         else
+            seg.bottomz = emax(realz1, realz2);
       }
 
       if((seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_FRONT_FLOOR) ||
          (!seg.line->frontside && seg.side->middleSkewType() == SKEW_MASKED_BACK_FLOOR))
       {
          seg.middlezstep = zstep;
-         seg.middlez = realz1 < realz2 ? realz1 : realz2;
+         if(zstep > 0.0f)
+            seg.middlez = emin(realz1, realz2);
+         else
+            seg.middlez = emax(realz1, realz2);
       }
    }
    else
