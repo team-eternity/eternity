995c04319fc1d354612069278ab21382512763d7
Ioan Chera <ioan.chera@gmail.com>
2024-08-24 13:00:33 +0300

    Carrying scrollers, including deep water, are slope aware now.

--- source/p_scroll.cpp
+++ source/p_scroll.cpp
@@ -70,6 +70,26 @@ static scrollerlist_t *scrollers;
 static PODCollection<sidelerpinfo_t> pScrolledSides;
 static PODCollection<seclerpinfo_t> pScrolledSectors;
 
+// Enable BOOM carrying scroller behaviour for thing on floor, while also handling sloped floors.
+inline static bool P_onCarryingFloor(const Mobj &thing, const surface_t &floor)
+{
+   if(!floor.slope)
+      return thing.z <= floor.height;
+   // Sloped: must rest on this slope.
+   return thing.zref.sector.floor && &thing.zref.sector.floor->srf.floor == &floor &&
+         thing.z == thing.zref.floor;
+}
+
+// Also for moving through BOOM-scrolling water surface
+inline static bool P_inCarryingWater(const Mobj &thing, const surface_t &floor,
+                                     const surface_t *waterlevel)
+{
+   if(!waterlevel)
+      return false;
+   fixed_t waterheight = waterlevel->getZAt(thing.x, thing.y);
+   return waterheight > floor.getZAt(thing.x, thing.y) && thing.z < waterheight;
+}
+
 IMPLEMENT_THINKER_TYPE(ScrollThinker)
 
 // killough 2/28/98:
@@ -120,6 +140,7 @@ void ScrollThinker::Think()
    fixed_t height, waterheight;  // killough 4/4/98: add waterheight
    msecnode_t *node;
    Mobj *thing;
+   const surface_t *watersurface;
 
    switch(this->type)
    {
@@ -168,6 +189,7 @@ void ScrollThinker::Think()
       waterheight = sec->heightsec != -1 &&
          sectors[sec->heightsec].srf.floor.height > height ?
          sectors[sec->heightsec].srf.floor.height : D_MININT;
+      watersurface = sec->heightsec != -1 ? &sectors[sec->heightsec].srf.floor : nullptr;
 
       // Move objects only if on floor or underwater,
       // non-floating, and clipped.
@@ -185,8 +207,8 @@ void ScrollThinker::Think()
          }
          if(!((thing = node->m_thing)->flags & MF_NOCLIP) &&
             !(thing->flags2 & MF2_NOTHRUST) &&
-            (!(thing->flags & MF_NOGRAVITY || thing->z > height) ||
-             thing->z < waterheight))
+            (!(thing->flags & MF_NOGRAVITY || !P_onCarryingFloor(*thing, sec->srf.floor)) ||
+             P_inCarryingWater(*thing, sec->srf.floor, watersurface)))
          {
             thing->momx += dx;
             thing->momy += dy;
