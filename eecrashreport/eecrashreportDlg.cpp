a8746bc6d8c56648d6f2327d966c39b80d16770a
Ioan Chera <ioan.chera@gmail.com>
2025-10-16 22:10:49 +0300

    Clang-format eecrashreport

--- eecrashreport/eecrashreportDlg.cpp
+++ eecrashreport/eecrashreportDlg.cpp
@@ -17,72 +17,74 @@ static char THIS_FILE[] = __FILE__;
 class CAboutDlg : public CDialog
 {
 public:
-	CAboutDlg();
+    CAboutDlg();
 
-// Dialog Data
-	//{{AFX_DATA(CAboutDlg)
-	enum { IDD = IDD_ABOUTBOX };
-	//}}AFX_DATA
+    // Dialog Data
+    //{{AFX_DATA(CAboutDlg)
+    enum
+    {
+        IDD = IDD_ABOUTBOX
+    };
+    //}}AFX_DATA
 
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CAboutDlg)
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	//}}AFX_VIRTUAL
-
-// Implementation
+    // ClassWizard generated virtual function overrides
+    //{{AFX_VIRTUAL(CAboutDlg)
 protected:
-	//{{AFX_MSG(CAboutDlg)
-	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
+    virtual void DoDataExchange(CDataExchange *pDX); // DDX/DDV support
+                                                     //}}AFX_VIRTUAL
+
+    // Implementation
+protected:
+    //{{AFX_MSG(CAboutDlg)
+    //}}AFX_MSG
+    DECLARE_MESSAGE_MAP()
 };
 
 CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
 {
-	//{{AFX_DATA_INIT(CAboutDlg)
-	//}}AFX_DATA_INIT
+    //{{AFX_DATA_INIT(CAboutDlg)
+    //}}AFX_DATA_INIT
 }
 
-void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+void CAboutDlg::DoDataExchange(CDataExchange *pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CAboutDlg)
-	//}}AFX_DATA_MAP
+    CDialog::DoDataExchange(pDX);
+    //{{AFX_DATA_MAP(CAboutDlg)
+    //}}AFX_DATA_MAP
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
-	//{{AFX_MSG_MAP(CAboutDlg)
-		// No message handlers
-	//}}AFX_MSG_MAP
+//{{AFX_MSG_MAP(CAboutDlg)
+// No message handlers
+//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CEECrashReportDlg dialog
 
-CEECrashReportDlg::CEECrashReportDlg(CWnd* pParent /*=NULL*/)
-	: CDialog(CEECrashReportDlg::IDD, pParent)
+CEECrashReportDlg::CEECrashReportDlg(CWnd *pParent /*=NULL*/) : CDialog(CEECrashReportDlg::IDD, pParent)
 {
-	//{{AFX_DATA_INIT(CEECrashReportDlg)
-		// NOTE: the ClassWizard will add member initialization here
-	//}}AFX_DATA_INIT
-	// Note that LoadIcon does not require a subsequent DestroyIcon in Win32
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+    //{{AFX_DATA_INIT(CEECrashReportDlg)
+    // NOTE: the ClassWizard will add member initialization here
+    //}}AFX_DATA_INIT
+    // Note that LoadIcon does not require a subsequent DestroyIcon in Win32
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
-void CEECrashReportDlg::DoDataExchange(CDataExchange* pDX)
+void CEECrashReportDlg::DoDataExchange(CDataExchange *pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CEECrashReportDlg)
-	DDX_Control(pDX, IDC_EDIT1, m_ErrorEditBox);
-	//}}AFX_DATA_MAP
+    CDialog::DoDataExchange(pDX);
+    //{{AFX_DATA_MAP(CEECrashReportDlg)
+    DDX_Control(pDX, IDC_EDIT1, m_ErrorEditBox);
+    //}}AFX_DATA_MAP
 }
 
 BEGIN_MESSAGE_MAP(CEECrashReportDlg, CDialog)
-	//{{AFX_MSG_MAP(CEECrashReportDlg)
-	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
-	//}}AFX_MSG_MAP
+//{{AFX_MSG_MAP(CEECrashReportDlg)
+ON_WM_SYSCOMMAND()
+ON_WM_PAINT()
+ON_WM_QUERYDRAGICON()
+//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -90,100 +92,100 @@ END_MESSAGE_MAP()
 
 BOOL CEECrashReportDlg::OnInitDialog()
 {
-   CDialog::OnInitDialog();
-   
-   // Add "About..." menu item to system menu.
-   
-   // IDM_ABOUTBOX must be in the system command range.
-   ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
-   ASSERT(IDM_ABOUTBOX < 0xF000);
-   
-   CMenu* pSysMenu = GetSystemMenu(FALSE);
-   if (pSysMenu != NULL)
-   {
-      CString strAboutMenu;
-      strAboutMenu.LoadString(IDS_ABOUTBOX);
-      if (!strAboutMenu.IsEmpty())
-      {
-         pSysMenu->AppendMenu(MF_SEPARATOR);
-         pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
-      }
-   }
-   
-   // Set the icon for this dialog.  The framework does this automatically
-   //  when the application's main window is not a dialog
-   SetIcon(m_hIcon, TRUE);			// Set big icon
-   SetIcon(m_hIcon, FALSE);		// Set small icon
-   
-   // haleyjd: extra initialization
-   HICON errorIcon = AfxGetApp()->LoadStandardIcon(MAKEINTRESOURCE(IDI_ERROR));
-   ((CStatic *)GetDlgItem(IDC_STATIC_ICON))->SetIcon(errorIcon);
-   
-   // load text from crashlog.txt file
-   LoadErrorFile();
-   
-   return TRUE;  // return TRUE  unless you set the focus to a control
+    CDialog::OnInitDialog();
+
+    // Add "About..." menu item to system menu.
+
+    // IDM_ABOUTBOX must be in the system command range.
+    ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+    ASSERT(IDM_ABOUTBOX < 0xF000);
+
+    CMenu *pSysMenu = GetSystemMenu(FALSE);
+    if(pSysMenu != NULL)
+    {
+        CString strAboutMenu;
+        strAboutMenu.LoadString(IDS_ABOUTBOX);
+        if(!strAboutMenu.IsEmpty())
+        {
+            pSysMenu->AppendMenu(MF_SEPARATOR);
+            pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+        }
+    }
+
+    // Set the icon for this dialog.  The framework does this automatically
+    //  when the application's main window is not a dialog
+    SetIcon(m_hIcon, TRUE);  // Set big icon
+    SetIcon(m_hIcon, FALSE); // Set small icon
+
+    // haleyjd: extra initialization
+    HICON errorIcon = AfxGetApp()->LoadStandardIcon(MAKEINTRESOURCE(IDI_ERROR));
+    ((CStatic *)GetDlgItem(IDC_STATIC_ICON))->SetIcon(errorIcon);
+
+    // load text from crashlog.txt file
+    LoadErrorFile();
+
+    return TRUE; // return TRUE  unless you set the focus to a control
 }
 
 void CEECrashReportDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
-	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
-	{
-		CAboutDlg dlgAbout;
-		dlgAbout.DoModal();
-	}
-	else
-	{
-		CDialog::OnSysCommand(nID, lParam);
-	}
+    if((nID & 0xFFF0) == IDM_ABOUTBOX)
+    {
+        CAboutDlg dlgAbout;
+        dlgAbout.DoModal();
+    }
+    else
+    {
+        CDialog::OnSysCommand(nID, lParam);
+    }
 }
 
 // If you add a minimize button to your dialog, you will need the code below
 //  to draw the icon.  For MFC applications using the document/view model,
 //  this is automatically done for you by the framework.
 
-void CEECrashReportDlg::OnPaint() 
+void CEECrashReportDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // device context for painting
+    if(IsIconic())
+    {
+        CPaintDC dc(this); // device context for painting
 
-		SendMessage(WM_ICONERASEBKGND, (WPARAM) dc.GetSafeHdc(), 0);
+        SendMessage(WM_ICONERASEBKGND, (WPARAM)dc.GetSafeHdc(), 0);
 
-		// Center icon in client rectangle
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+        // Center icon in client rectangle
+        int   cxIcon = GetSystemMetrics(SM_CXICON);
+        int   cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// Draw the icon
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialog::OnPaint();
-	}
+        // Draw the icon
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CDialog::OnPaint();
+    }
 }
 
 // The system calls this to obtain the cursor to display while the user drags
 //  the minimized window.
 HCURSOR CEECrashReportDlg::OnQueryDragIcon()
 {
-	return (HCURSOR) m_hIcon;
+    return (HCURSOR)m_hIcon;
 }
 
-void CEECrashReportDlg::OnOK() 
+void CEECrashReportDlg::OnOK()
 {
-   // copy data in message box to the clipboard
-   m_ErrorEditBox.SendMessage(EM_SETSEL, 0, filelen);
-   m_ErrorEditBox.SendMessage(WM_COPY,   0, 0);
-   m_ErrorEditBox.SendMessage(EM_SETSEL, 0, 0);
-   
-   LaunchBrowser();
-   
-   //CDialog::OnOK();
+    // copy data in message box to the clipboard
+    m_ErrorEditBox.SendMessage(EM_SETSEL, 0, filelen);
+    m_ErrorEditBox.SendMessage(WM_COPY, 0, 0);
+    m_ErrorEditBox.SendMessage(EM_SETSEL, 0, 0);
+
+    LaunchBrowser();
+
+    // CDialog::OnOK();
 }
 
 //
@@ -193,63 +195,63 @@ void CEECrashReportDlg::OnOK()
 //
 void CEECrashReportDlg::LoadErrorFile()
 {
-   char *str;
-   FILE *f;
-   char moduleName[2*MAX_PATH];
-   char *filename;
+    char *str;
+    FILE *f;
+    char  moduleName[2 * MAX_PATH];
+    char *filename;
 
-   memset(moduleName, 0, sizeof(moduleName));
+    memset(moduleName, 0, sizeof(moduleName));
 
-   GetModuleFileName(NULL, moduleName, MAX_PATH);
+    GetModuleFileName(NULL, moduleName, MAX_PATH);
 
-   filename = strrchr(moduleName, '\\');
+    filename = strrchr(moduleName, '\\');
 
-   if(filename)
-   {
-      char *tmp = filename;
+    if(filename)
+    {
+        char *tmp = filename;
 
-      while(*filename != '\0')
-         *filename++ = '\0';
+        while(*filename != '\0')
+            *filename++ = '\0';
 
-      strcat(moduleName, "\\crashlog.txt");
-   }      
-   else
-   {
-      memset(moduleName, 0, sizeof(moduleName));
-      strcpy(moduleName, "crashlog.txt");
-   }
+        strcat(moduleName, "\\crashlog.txt");
+    }
+    else
+    {
+        memset(moduleName, 0, sizeof(moduleName));
+        strcpy(moduleName, "crashlog.txt");
+    }
 
-   if(!(f = fopen(moduleName, "rb")))
-   {
-      AfxMessageBox("crashlog.txt not found.", MB_OK | MB_ICONEXCLAMATION);
-      return;
-   }
+    if(!(f = fopen(moduleName, "rb")))
+    {
+        AfxMessageBox("crashlog.txt not found.", MB_OK | MB_ICONEXCLAMATION);
+        return;
+    }
 
-   // seek to end of file and get offset
-   fseek(f, 0, SEEK_END);
-   filelen = (size_t)ftell(f);
-   rewind(f);
+    // seek to end of file and get offset
+    fseek(f, 0, SEEK_END);
+    filelen = (size_t)ftell(f);
+    rewind(f);
 
-   if(filelen == 0)
-   {
-      AfxMessageBox("crashlog.txt is empty.", MB_OK | MB_ICONEXCLAMATION);
-      fclose(f);
-      return;
-   }
+    if(filelen == 0)
+    {
+        AfxMessageBox("crashlog.txt is empty.", MB_OK | MB_ICONEXCLAMATION);
+        fclose(f);
+        return;
+    }
 
-   // read file
-   str = new char [filelen + 1];
-   memset(str, 0, filelen + 1);
-   fread(str, 1, filelen, f);
+    // read file
+    str = new char[filelen + 1];
+    memset(str, 0, filelen + 1);
+    fread(str, 1, filelen, f);
 
-   // done with file
-   fclose(f);
+    // done with file
+    fclose(f);
 
-   // set string to message box
-   m_ErrorEditBox.SetWindowText(str);
+    // set string to message box
+    m_ErrorEditBox.SetWindowText(str);
 
-   // done with string (I hope)
-   delete [] str;
+    // done with string (I hope)
+    delete[] str;
 }
 
 //
@@ -260,7 +262,5 @@ void CEECrashReportDlg::LoadErrorFile()
 //
 void CEECrashReportDlg::LaunchBrowser()
 {
-   ShellExecute(NULL, "open", 
-                "https://www.doomworld.com/forum/25-eternity/?do=add", 
-                NULL, NULL, SW_SHOWNORMAL);
+    ShellExecute(NULL, "open", "https://www.doomworld.com/forum/25-eternity/?do=add", NULL, NULL, SW_SHOWNORMAL);
 }
--- eecrashreport/resource.h
+++ eecrashreport/resource.h
@@ -11,7 +11,7 @@
 #define IDC_STATIC_ICON                 1002
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
